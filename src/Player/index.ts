5b3a867e2e38bfe85a06aed3882cb7f6810a0f9a
Andrii Kicha <akicha@star.global>
2023-08-19 16:41:38 -0600

    update the player image

--- src/Player/index.ts
+++ src/Player/index.ts
@@ -1,44 +1,119 @@
 import { getContext, getCanvas } from '../Canvas';
 import { keyboard } from '../Keyboard';
-import { TILE_HEIGHT, TILE_WIDTH } from '../Map';
 import { Player } from './interfaces';
 import { isPlayerColliding } from '../PlayerCollisionController';
+import dinaStand from '../../static/assets/images/Dina_stand.png';
+import dinaJump from '../../static/assets/images/Dino_jump.png';
 
-const PLAYER_VELOCITY_X_CAP = 10;
+const PLAYER_VELOCITY_X_CAP = 7;
 const PLAYER_VELOCITY_Y_CAP = 25;
 const JUMP_VELOCITY = 25;
+// actually it's the index 0 - 2 meaning there are 3 images
+const PLAYER_IMAGE_NUM_FRAMES = 2;
 
 export const player: Player = {
   x: 50,
   y: 200,
-  width: TILE_WIDTH,
-  height: TILE_HEIGHT,
+  width: 64,
+  height: 64,
   velocity: {
     x: PLAYER_VELOCITY_X_CAP,
     y: 0,
   },
+  image: null,
+  imageJump: null,
+  currentFrame: 0,
+  frameBuffer: 4,
+  elapsedFrames: 0,
+  moving: false,
+  jumps: false,
 };
+
+export async function initPlayerImages() {
+  const image = new Image();
+  const imageJump = new Image();
+
+  image.src = dinaStand;
+  imageJump.src = dinaJump;
+
+  await new Promise((resolve, reject) => {
+    image.onload = resolve;
+    image.onerror = reject;
+  });
+
+  await new Promise((resolve, reject) => {
+    imageJump.onload = resolve;
+    imageJump.onerror = reject;
+  });
+
+  player.image = image;
+  player.imageJump = imageJump;
+}
 
 export function drawPlayer() {
   const ctx = getContext();
 
-  ctx.fillStyle = '#ff0000';
-  ctx.fillRect(player.x, player.y, player.width, player.height);
+  if (!player.image || !player.imageJump) {
+    throw new Error(
+      'Oops, you forgot to initialize your player image before drawing the player'
+    );
+  }
+
+  if (!player.moving || player.jumps) {
+    player.currentFrame = 0;
+  }
+
+  const cropBox = {
+    position: {
+      x: player.currentFrame * player.width,
+      y: 0,
+    },
+    width: player.width,
+    height: player.height,
+  };
+
+  ctx.drawImage(
+    player.jumps ? player.imageJump : player.image,
+    cropBox.position.x,
+    cropBox.position.y,
+    cropBox.width,
+    cropBox.height,
+    player.x,
+    player.y,
+    player.width,
+    player.height
+  );
+
+  player.elapsedFrames += 1;
+
+  if (player.elapsedFrames % player.frameBuffer === 0) {
+    player.currentFrame += 1;
+  }
+  if (player.currentFrame > PLAYER_IMAGE_NUM_FRAMES) {
+    player.currentFrame = 0;
+  }
 }
 
 export function movePlayer() {
   const canvas = getCanvas();
 
   player.y += player.velocity.y;
+  player.moving = false;
+
+  // for some reason player is in the air
+  // update the jumps state
+  player.jumps = player.velocity.y > 0;
 
   if (keyboard.pressed.KeyD) {
     if (player.x + player.width < canvas.width) {
+      player.moving = true;
       player.x += player.velocity.x;
     }
   }
 
   if (keyboard.pressed.KeyA) {
     if (player.x > 0) {
+      player.moving = true;
       player.x -= player.velocity.x;
     }
   }

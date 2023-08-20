5a9164935b79a6b232606c84249cb39dae663ac9
Andrii Beha <95135544+andriibeha@users.noreply.github.com>
2023-08-20 10:26:59 -0600

    add all sfx audio files

--- src/Audio/index.ts
+++ src/Audio/index.ts
@@ -1,18 +1,54 @@
 export function playSFX(clip: string): void {
-    let audioSFX = new Audio()
-    switch(clip) { 
-        case 'jump': { 
-            audioSFX.src = './assets/audio/sfx/sfx_jump.mp3'
-            break; 
-        } 
-        case 'collect': { 
-            audioSFX.src = './assets/audio/sfx/sfx_collect.mp3'
-            break; 
-        } 
-        default: { 
-            audioSFX.src = ''
-            break; 
-        } 
+  let audioSFX = new Audio();
+  switch (clip) {
+    case 'jump': {
+      audioSFX.src = './assets/audio/sfx/sfx_jump.mp3';
+      break;
     }
-    audioSFX.play()
+    case 'collect': {
+      audioSFX.src = './assets/audio/sfx/sfx_collect.mp3';
+      break;
+    }
+    case 'gameOverLost': {
+      audioSFX.src = './assets/audio/sfx/sfx_game_over_lost.mp3';
+      break;
+    }
+    case 'gameOverWin': {
+      audioSFX.src = './assets/audio/sfx/sfx_game_over_win.mp3';
+      break;
+    }
+    case 'gameStart': {
+      audioSFX.src = './assets/audio/sfx/sfx_game_start.mp3';
+      break;
+    }
+    case 'lastCollect': {
+      audioSFX.src = './assets/audio/sfx/sfx_last_collect.mp3';
+      break;
+    }
+    case 'levelUp': {
+      audioSFX.src = './assets/audio/sfx/sfx_level_up.mp3';
+      break;
+    }
+    case 'press_button': {
+      audioSFX.src = './assets/audio/sfx/sfx_press_button.mp3';
+      break;
+    }
+    case 'timeUp': {
+      audioSFX.src = './assets/audio/sfx/sfx_time_up.mp3';
+      break;
+    }
+    case 'timeWarning': {
+      audioSFX.src = './assets/audio/sfx/sfx_time_warning.mp3';
+      break;
+    }
+    case 'trap': {
+      audioSFX.src = './assets/audio/sfx/sfx_trap.mp3';
+      break;
+    }
+    default: {
+      audioSFX.src = '';
+      break;
+    }
+  }
+  audioSFX.play();
 }
